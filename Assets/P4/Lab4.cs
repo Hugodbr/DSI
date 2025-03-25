b6c8c26b3f816e6cc5d00641956eff1e7b9dad02
TelmoK <telmo.kiss@gmail.com>
2025-03-25 17:44:05 +0100

    Creando Custom Control

--- Assets/P4/Lab4.cs
+++ Assets/P4/Lab4.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class Lab4 : MonoBehaviour
+public class Lab4 : VisualElement
 {
-    // Start is called before the first frame update
-    void Start()
+    public new class UxmlFactory : UxmlFactory<Lab4> { };
+
+    List<VisualElement> items;
+
+    int estado;
+
+    public int Estado
     {
-        
+        get => estado;
+        set
+        {
+            estado = value;
+            setDisplay();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public Lab4()
     {
-        
+        items = new List<VisualElement>(5);
+
+        foreach(VisualElement item in items)
+        {
+            item.style.width = 100;
+            item.style.height = 100;
+            item.style.backgroundColor = Color.gray;
+
+            //item.AddToClassList("blabla");
+
+            hierarchy.Add(item);
+        }
+    }
+
+    private void setDisplay()
+    {
+
     }
 }
