8f256498daa3f595e17e9053b14ac055a6e905da
TelmoK <telmo.kiss@gmail.com>
2025-03-27 16:37:30 +0100

    Practica 4

--- Assets/P4/Lab4.cs
+++ Assets/P4/Lab4.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEditor.Progress;
 
 public class Lab4 : VisualElement
 {
-    public new class UxmlFactory : UxmlFactory<Lab4> { };
-
     List<VisualElement> items;
 
     int estado;
@@ -23,15 +22,24 @@ public class Lab4 : VisualElement
 
     public Lab4()
     {
-        items = new List<VisualElement>(5);
+        Label l = new Label();
+        l.text = "Playas: ";
+        hierarchy.Add(l);
+
+        items = new List<VisualElement>(5)
+        {
+            new VisualElement(),
+            new VisualElement(),
+            new VisualElement(),
+            new VisualElement(),
+            new VisualElement()
+        };
 
         foreach(VisualElement item in items)
         {
-            item.style.width = 100;
-            item.style.height = 100;
-            item.style.backgroundColor = Color.gray;
-
-            //item.AddToClassList("blabla");
+            item.style.width = 50;
+            item.style.height = 50;
+            item.style.backgroundImage = Resources.Load<Texture2D>("praia-do-forte");
 
             hierarchy.Add(item);
         }
@@ -39,6 +47,31 @@ public class Lab4 : VisualElement
 
     private void setDisplay()
     {
+        foreach (VisualElement item in items)
+        {
+            item.style.opacity = 0.5f;
+        }
 
+        int i = 0;
+        foreach (VisualElement item in items)
+        {
+            if (i >= Estado) break;
+            item.style.opacity = 1;
+            i++;
+        }
+    }
+
+    public new class UxmlFactory : UxmlFactory<Lab4, UxmlTraits> { };
+
+    public new class UxmlTraits : VisualElement.UxmlTraits
+    {
+        UxmlIntAttributeDescription myEstado = new UxmlIntAttributeDescription { name = "estado", defaultValue = 0};
+
+        public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+        {
+            base.Init(ve, bag, cc);
+            var semaforo = ve as Lab4;
+            semaforo.Estado = myEstado.GetValueFromBag(bag, cc);
+        }
     }
 }
